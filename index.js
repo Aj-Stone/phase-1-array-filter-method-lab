7e6f74ad2d05d2c61b0e9883a1c77e1361c3ec0d
Alex Atsitty <atsitty.alex1128@gmail.com>
2022-03-02 12:34:00 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array,string){
+    let matching = array.filter(name => name===string || name===string.toLowerCase())
+    return matching
+}
+
+function fuzzyMatch(array, string){
+    let sameLetter = array.filter(letter => letter[0]===string[0])
+    return sameLetter
+}
+
+function matchName(drivers,string){
+    let matchingName = drivers.filter(driver => driver.name === string)
+    return matchingName
+}
